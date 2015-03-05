8f60d171fca65c49dca4685444d39825dfa336e8
Alex Mastro <alex.mastro@cree.com>
2015-03-05 17:47:18 -0500

    Add locate-current-file-in-explorer

--- extensions.el
+++ extensions.el
@@ -38,4 +38,20 @@ save the pointer marker if tag is found"
         (goto-char pos))
       (set-marker marker nil nil))))
 
+(defun locate-current-file-in-explorer ()
+  (interactive)
+  (cond
+   ;; In buffers with file name
+   ((buffer-file-name)
+    (shell-command (concat "start explorer /e,/select,\"" (replace-regexp-in-string "/" "\\\\" (buffer-file-name)) "\"")))
+   ;; In dired mode
+   ((eq major-mode 'dired-mode)
+    (shell-command (concat "start explorer /e,\"" (replace-regexp-in-string "/" "\\\\" (dired-current-directory)) "\"")))
+   ;; In eshell mode
+   ((eq major-mode 'eshell-mode)
+    (shell-command (concat "start explorer /e,\"" (replace-regexp-in-string "/" "\\\\" (eshell/pwd)) "\"")))
+   ;; Use default-directory as last resource
+   (t
+    (shell-command (concat "start explorer /e,\"" (replace-regexp-in-string "/" "\\\\" default-directory) "\"")))))
+
 (provide 'my-extensions)
