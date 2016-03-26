fc8f6b5e511f2c55dc45bc158a1782f2f60d54d6
Alex Mastro <stro@Alexs-MBP.att.net>
2016-03-26 00:23:34 -0700

    Remove dead code

--- init.el
+++ init.el
@@ -96,10 +96,6 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (delete 'company-semantic company-backends)
-;;(define-key c-mode-base-map [(tab)] 'company-complete)
-;; (setq company-minimum-prefix-length 2)
-;; (setq company-idle-delay 0)
-;; (setq company-dabbrev-downcase nil)
 
 (setq org-src-fontify-natively t)
 
@@ -299,9 +295,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-type-face ((t (:foreground "#161616" :underline nil)))))
