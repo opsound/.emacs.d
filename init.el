771ab227bfca43e57fe6b4cd1292e3c36b155d4c
Alex Mastro <stro@Alexs-MacBook-Pro.local>
2016-12-08 13:18:55 -0800

    Delete irony-mode

--- init.el
+++ init.el
@@ -241,26 +241,6 @@
 ;;   :config
 ;;   (add-hook 'prog-mode-hook (lambda () (relative-line-numbers-mode))))
 
-(use-package irony
-  :config
-  (add-hook 'c++-mode-hook 'irony-mode)
-  (add-hook 'c-mode-hook 'irony-mode)
-  (add-hook 'objc-mode-hook 'irony-mode)
-
-  ;; replace the `completion-at-point' and `complete-symbol' bindings in
-  ;; irony-mode's buffers by irony-mode's function
-  (defun my-irony-mode-hook ()
-    (define-key irony-mode-map [remap completion-at-point]
-      'irony-completion-at-point-async)
-    (define-key irony-mode-map [remap complete-symbol]
-      'irony-completion-at-point-async))
-  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-  (use-package company-irony
-    :config
-    (push 'company-irony company-backends)))
-
 (use-package flycheck)
 
 (use-package rtags
