45fa75ea7b0a1f6c4ffe4640f02563a5e999f962
Alex Mastro <amastro@fb.com>
2019-09-11 13:12:55 -0700

    Delete dead rtags, irony config

--- init.el
+++ init.el
@@ -278,41 +278,6 @@
               "t" 'dumb-jump-back))
           (list 'python-mode 'c-mode 'c++-mode 'objc-mode)))
 
-;; (use-package rtags
-;;   :config
-;;   (rtags-enable-standard-keybindings)
-;;   (setq rtags-rc-log-enabled t)
-;;   (setq rtags-autostart-diagnostics t)
-;;   (rtags-diagnostics)
-;;   (setq rtags-completions-enabled t)
-;;   (push 'company-rtags company-backends)
-;;   (add-hook 'c-mode-common-hook 'rtags-start-process-unless-running)
-;;   (mapcar (lambda (mode)
-;;             (evil-leader/set-key-for-mode mode
-;;               "k" 'rtags-find-symbol-at-point
-;;               "t" 'rtags-location-stack-back
-;;               "r n" 'rtags-next-match
-;;               "r p" 'rtags-previous-match
-;;               "r r" 'rtags-find-references-at-point
-;;               "r R" 'rtags-rename-symbol))
-;;           (list 'c-mode 'c++-mode 'objc-mode)))
-
-;; (use-package irony
-;;   :config
-;;   (add-hook 'c++-mode-hook 'irony-mode)
-;;   (add-hook 'c-mode-hook 'irony-mode)
-;;   (add-hook 'objc-mode-hook 'irony-mode)
-
-;;   ;; replace the `completion-at-point' and `complete-symbol' bindings in
-;;   ;; irony-mode's buffers by irony-mode's function
-;;   (defun my-irony-mode-hook ()
-;;     (define-key irony-mode-map [remap completion-at-point]
-;;       'irony-completion-at-point-async)
-;;     (define-key irony-mode-map [remap complete-symbol]
-;;       'irony-completion-at-point-async))
-;;   (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-;;   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
-
 (use-package markdown-mode)
 (use-package org)
 (use-package wgrep)
